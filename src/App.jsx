42c9ab583d6e62d7af79a3c8c92f957eca551ad5
loverboy <loverboykosu@gmail.com>
2025-03-16 09:53:52 +0900

    feat: Translator frame work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-
+import Translator from "./components/Translator";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-blue-500">hello</h1>
-      <button className="bg-blue-500">Button A</button>
+      <Translator />
     </>
   );
 }
---
+++ src/components/Translator.jsx
@@ -0,0 +1,8 @@
+const Translator = () => {
+  return (
+    <>
+      <h1 className="bg-gray-800 text-white text-center p-3">Translator App</h1>
+    </>
+  );
+};
+export default Translator;
