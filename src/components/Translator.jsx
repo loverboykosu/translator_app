39e229fbc0f4dc8e7c0a38bea7c5f3f5c56b29d4
loverboy <loverboykosu@gmail.com>
2025-03-16 10:11:27 +0900

    feat: add frame work

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -2,6 +2,16 @@ const Translator = () => {
   return (
     <>
       <h1 className="bg-gray-800 text-white text-center p-3">Translator App</h1>
+      <div className="flex flex-row">
+        {/* オリジナル言語 */}
+        <div className="origin basis-1/2 text-center border-2 border-solid m-3">
+          <h2>翻訳対象</h2>
+        </div>
+        {/* 翻訳後 */}
+        <div className="translated basis-1/2 text-center border-2 border-solid m-3">
+          <h2>翻訳結果</h2>
+        </div>
+      </div>
     </>
   );
 };
