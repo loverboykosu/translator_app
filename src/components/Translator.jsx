bb7f70ed1362bfe40880757cc7ca689b88f1de38
loverboy <loverboykosu@gmail.com>
2025-03-16 10:20:07 +0900

    feat: add textarea

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -4,12 +4,24 @@ const Translator = () => {
       <h1 className="bg-gray-800 text-white text-center p-3">Translator App</h1>
       <div className="flex flex-row">
         {/* オリジナル言語 */}
-        <div className="origin basis-1/2 text-center border-2 border-solid m-3">
-          <h2>翻訳対象</h2>
+        <div className="origin basis-1/2 text-center m-3">
+          <h2 className="border-2 border-solid m-3">翻訳対象</h2>
+          <textarea
+            className="border-2 border-solid"
+            name="postContent"
+            rows={4}
+            cols={40}
+          />
         </div>
         {/* 翻訳後 */}
-        <div className="translated basis-1/2 text-center border-2 border-solid m-3">
-          <h2>翻訳結果</h2>
+        <div className="origin basis-1/2 text-center m-3">
+          <h2 className="border-2 border-solid m-3">翻訳結果</h2>
+          <textarea
+            className="border-2 border-solid"
+            name="postContent"
+            rows={4}
+            cols={40}
+          />
         </div>
       </div>
     </>
